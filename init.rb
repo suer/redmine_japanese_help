722a77438130f3c03810a49f09e0a6c604447f69
suer <sue@nagoya-u.jp>
2010-02-06 23:12:26 +0900

    write auther and description

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_japanese_help do
   name 'Redmine Japanese Help plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'suer'
+  description 'replace help link to Redmine.JP'
   version '0.0.1'
   delete_menu_item :top_menu, :help
   menu :top_menu, :help, 'http://redmine.jp/guide/', :last => true
