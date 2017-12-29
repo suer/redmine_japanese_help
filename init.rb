1af1b7e407421936d036b375d05da5ae1e933e1b
MAEDA Go <maeda@farend.jp>
2017-12-29 04:41:08 +0000

    Update the URL of Japanese help.

--- init.rb
+++ init.rb
@@ -6,5 +6,5 @@ Redmine::Plugin.register :redmine_japanese_help do
   description 'replace help link to Redmine.JP'
   version '0.0.1'
   delete_menu_item :top_menu, :help
-  menu :top_menu, :help, 'http://redmine.jp/guide/', :last => true
+  menu :top_menu, :help, 'http://guide.redmine.jp/', :last => true
 end
